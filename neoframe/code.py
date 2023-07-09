44018d516458ca129d3c2361ec53aef9c0d0c365
Nick Chalko <nick@chalko.com>
2023-07-09 15:38:26 -0700

    More distance debugging

--- neoframe/code.py
+++ neoframe/code.py
@@ -8,8 +8,8 @@ from analogio import AnalogIn
 
 print('start')
 NUMPIXELS = 48# Update this to match the number of LEDs.
-SPEED = 0.05  # Increase to slow down the rainbow. Decrease to speed it up.
 BRIGHTNESS = 0.99  # A number between 0.0 and 1.0, where 0.0 is off, and 1.0 is max.
+SPEED = 0.05  # Increase to slow down the rainbow. Decrease to speed it up.
 TOP_LIGHT_PIN = board.NEOPIXEL7
 LEFT_LED_PIN = board.NEOPIXEL0
 
@@ -44,19 +44,27 @@ old_value = pir.value
 while True:
     pir_value = pir.value
     distRaw  = dist_in.value
-    #print("A0: %f" % distRaw)
+    # print("A0: %f" % distRaw)
+    # time.sleep(.1)
+    dist_of_1 = 1- distRaw / 65535
+    dist_of_255 = int(255 - (distRaw * 255) / 65535)
 
     if pir_value:
         # PIR is detecting movement! Turn on LED.
         # Check if this is the first time movement was
         # detected and print a message!
+        pixels.fill(RED)
+        pixels.show()
+        # print("A0: %f" % dist_of_255)
         if not old_value:
             print('Motion detected!')
             distRaw  = dist_in.value
-            print("A0: %f" % distRaw)  # write raw value to REPL
+            print("distRaw: %f" % distRaw)
+            print("dist_of_1: %f" % dist_of_1)
+            print("dist_of_255: %f" % dist_of_255)
             board_led.value = True
-            pixels.fill(RED)
-            pixels.show()
+            #pixels.fill((dist_of_255,0,0))
+            #pixels.show()
 
     else:
         # PIR is not detecting movement. Turn off LED.
@@ -65,9 +73,6 @@ while True:
         if old_value:
             print('Motion ended!')
             board_led.value = False
-            pixels.fill(BLUE)
-            pixels.show()
-            time.sleep(.5)
             pixels.fill(BLACK)
             pixels.show()
     old_value = pir_value
